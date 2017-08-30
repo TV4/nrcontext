418ac9c43791846a9ea2446c739015657ff309b1
Peter Hellberg <peter@c7.se>
2017-08-31 00:04:06 +0200

    Add documentation

---
+++ nrmock/doc.go
@@ -0,0 +1,16 @@
+/*
+
+Package nrmock contains mock implementations of newrelic.Application and newrelic.Transaction
+
+Links
+
+The newrelic.Application interface is defined here
+
+ https://github.com/newrelic/go-agent/blob/master/application.go
+
+And the newrelic.Transaction interface is defined here
+
+ https://github.com/newrelic/go-agent/blob/master/transaction.go
+
+*/
+package nrmock
